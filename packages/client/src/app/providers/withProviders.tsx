07d2ddcaa677b6e9fa77458edcf13ec7e35cb370
Nikolay Kamenev <kamenik1@icloud.com>
2022-12-24 10:59:06 +0300

    feat: added redux

--- packages/client/src/app/providers/withProviders.tsx
+++ packages/client/src/app/providers/withProviders.tsx
@@ -2,10 +2,11 @@ import { FC, JSXElementConstructor, PropsWithChildren } from 'react'
 
 import { WithRouter } from './withRouter'
 import { WithTheme } from './withTheme'
+import { WithRedux } from './withRedux'
 
 type TCComponents = Array<JSXElementConstructor<PropsWithChildren<unknown>>>
 
-const components: TCComponents = [WithRouter, WithTheme]
+const components: TCComponents = [WithRouter, WithTheme, WithRedux]
 
 export const WithProviders: FC<PropsWithChildren> = ({ children }) => {
   return (

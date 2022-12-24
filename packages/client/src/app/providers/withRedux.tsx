49b93ffc20b6858a162a09e928be63de4d950e47
Nikolay Kamenev <kamenik1@icloud.com>
2022-12-24 11:04:40 +0300

    fix: test

--- packages/client/src/app/providers/withRedux.tsx
+++ packages/client/src/app/providers/withRedux.tsx
@@ -1,8 +1,8 @@
-import {FC, PropsWithChildren} from "react";
-import {Provider} from "react-redux";
+import { FC, PropsWithChildren } from 'react'
+import { Provider } from 'react-redux'
 
-import {store} from "entities/store";
+import { store } from '../../entities/store'
 
-export const WithRedux: FC<PropsWithChildren> = ({children}) => {
-    return <Provider store={store}>{children}</Provider>;
-};
+export const WithRedux: FC<PropsWithChildren> = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>
+}

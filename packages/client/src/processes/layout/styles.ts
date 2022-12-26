1bb675ffdac9e3261e31602659ee67688e128359
Nikolay Kamenev <kamenik1@icloud.com>
2022-12-26 12:56:00 +0300

    feat: fix px-rem

--- packages/client/src/processes/layout/styles.ts
+++ packages/client/src/processes/layout/styles.ts
@@ -4,7 +4,7 @@ import { Layout } from 'antd'
 export const FullLayout = styled(Layout)`
   height: 100%;
   width: 100%;
-  max-width: 800px;
+  max-width: 50rem;
   margin: 20px auto;
   padding: 0 10px;
 `

e2b9f9f851d63b2f572dbe2ef0eb138953c07ae9
e.zaborshchikov <e.zaborshchikov@yandex.ru>
2022-12-29 17:56:28 +0300

    add game page

---
+++ packages/client/src/pages/game/ui.tsx
@@ -0,0 +1,8 @@
+import { FC, PropsWithChildren } from 'react'
+// import { }
+
+export const Game: FC<PropsWithChildren> = () => {
+  return (
+    <canvas></canvas>
+  );
+};
--- packages/client/src/processes/routes/routes.tsx
+++ packages/client/src/processes/routes/routes.tsx
@@ -2,11 +2,12 @@ import { RouteObject } from 'react-router-dom'
 import { routesPath } from './routesPath'
 import { Login } from 'pages/login'
 import { Start } from 'pages/start'
+import { Game } from 'pages/game'
 
 export const routes: RouteObject[] = [
   { path: routesPath.home, element: <Start /> },
   { path: routesPath.login, element: <Login /> },
   { path: routesPath.profile, element: <div>PROFILE</div> },
   { path: routesPath.logout, element: <div>LOGOUT</div> },
-  { path: routesPath.game, element: <div>GAME</div> }
+  { path: routesPath.game, element: <Game /> }
 ]

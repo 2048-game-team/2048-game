8746ede15bae6fb3b4e46474435b2b85a81e40a0
e.zaborshchikov <e.zaborshchikov@yandex.ru>
2022-12-29 23:00:38 +0300

    add page elements

--- packages/client/src/pages/game/ui.tsx
+++ packages/client/src/pages/game/ui.tsx
@@ -1,8 +1,27 @@
-import { FC, PropsWithChildren } from 'react'
-// import { }
+import { FC, PropsWithChildren, useRef } from 'react'
+import { Button, Col, Row, Typography } from 'antd'
+import { GameSpace } from './styles';
+
+const restart = () => { 
+  console.log('Restart game.')
+}
+
+const height = 500;
+const width = 500;
+const score = 2484;
 
 export const Game: FC<PropsWithChildren> = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
   return (
-    <canvas></canvas>
+    <Row>
+      <Col span={24}>
+        <GameSpace direction="vertical" align="center" size="large">
+          <Typography.Title level={5}>Score: { score}</Typography.Title>
+          <canvas ref={canvasRef} height={height} width={ width} />
+          <Button onClick={restart}>Рестарт</Button>
+        </GameSpace>
+      </Col>
+    </Row>
   );
 };

3516f4b85968e5ead90a0afc1e07c5d57c39028c
e.zaborshchikov <e.zaborshchikov@yandex.ru>
2023-01-10 13:45:46 +0300

    add canvas element

--- packages/client/src/pages/game/ui.tsx
+++ packages/client/src/pages/game/ui.tsx
@@ -1,4 +1,4 @@
-import { FC, PropsWithChildren, useRef } from 'react'
+import { FC, PropsWithChildren, useEffect, useRef } from 'react'
 import { Button, Col, Row, Typography } from 'antd'
 import { GameSpace } from './styles';
 
@@ -6,22 +6,45 @@ const restart = () => {
   console.log('Restart game.')
 }
 
-const height = 500;
-const width = 500;
-const score = 2484;
+const HEIGHT = 400;
+const WIDTH = 500;
+const SCORE = 2484;
 
-export const Game: FC<PropsWithChildren> = () => {
+const GAME_STATE = [
+  [0, 2, 4],
+  [0, 0, 4],
+  [0, 0, 0]
+];
+
+const drawGameField = (ctx: CanvasRenderingContext2D, GAME_STATE: number[][]) => {
+  ctx.beginPath();
+  ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+  ctx.stroke();
+  console.log(GAME_STATE)
+};
+
+type GameCanvasProps = { width?: number, height?: number };
+
+const GameCanvas: FC<GameCanvasProps> = ({ width = WIDTH, height = HEIGHT}) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
+  useEffect(() => {
+    const ctx: CanvasRenderingContext2D | null | undefined = canvasRef.current?.getContext('2d');
+    if (ctx) { 
+      drawGameField(ctx, GAME_STATE);
+    }
+  }, []);
+
+  return <canvas ref={canvasRef} height={height} width={ width} />
+};
+
+export const Game: FC<PropsWithChildren> = () => {
+
   return (
-    <Row>
-      <Col span={24}>
-        <GameSpace direction="vertical" align="center" size="large">
-          <Typography.Title level={5}>Score: { score}</Typography.Title>
-          <canvas ref={canvasRef} height={height} width={ width} />
-          <Button onClick={restart}>Рестарт</Button>
-        </GameSpace>
-      </Col>
-    </Row>
+    <GameSpace direction="vertical" align="center" size="large">
+      <Typography.Title level={5}>Score: {SCORE}</Typography.Title>
+      <GameCanvas width={WIDTH} height={HEIGHT} />
+      <Button onClick={restart}>Рестарт</Button>
+    </GameSpace>
   );
 };

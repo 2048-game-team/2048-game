431da4f99156f0081461ba65846d49563c9e1b44
Evgeniy.Zaborshchikov <49640784+jacksonrr3@users.noreply.github.com>
2023-01-12 16:09:47 +0300

    feat: 2048-40 Добавлено представление игры (#14)

--- packages/client/src/processes/routes/routes.tsx
+++ packages/client/src/processes/routes/routes.tsx
@@ -4,6 +4,7 @@ import { Login } from 'pages/login'
 import { Profile } from 'pages/profile'
 import { Forum } from 'pages/forum'
 import { Start } from 'pages/start'
+import { Game } from 'pages/game'
 
 export const routes: RouteObject[] = [
   { path: routesPath.home, element: <Start /> },
@@ -11,5 +12,5 @@ export const routes: RouteObject[] = [
   { path: routesPath.profile, element: <Profile /> },
   { path: routesPath.forum, element: <Forum /> },
   { path: routesPath.logout, element: <div>LOGOUT</div> },
-  { path: routesPath.game, element: <div>GAME</div> },
+  { path: routesPath.game, element: <Game /> }
 ]

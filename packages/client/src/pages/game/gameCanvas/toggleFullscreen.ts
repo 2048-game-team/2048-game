3f87c9c3c4089a8d56ed8c78fd41fdad757ee941
e.zaborshchikov <e.zaborshchikov@yandex.ru>
2023-01-14 23:27:49 +0300

    add opera and firefox support

--- packages/client/src/pages/game/gameCanvas/toggleFullscreen.ts
+++ packages/client/src/pages/game/gameCanvas/toggleFullscreen.ts
@@ -1,7 +1,48 @@
-export const toggleFullscreen = (element: HTMLElement) => {
-  if (!document.fullscreenElement) {
+interface DocumentWithFullscreen extends Document {
+  mozFullScreenElement?: Element;
+  webkitFullscreenElement?: Element;
+  mozCancelFullScreen?: () => void;
+  webkitExitFullscreen?: () => void;
+}
+
+function isFullScreen(): boolean {
+  const doc = document as DocumentWithFullscreen;
+  return !!(
+    doc.fullscreenElement ||
+    doc.mozFullScreenElement ||
+    doc.webkitFullscreenElement
+  );
+}
+
+interface DocumentElementWithFullscreen extends HTMLElement {
+  mozRequestFullScreen?: () => void;
+  webkitRequestFullscreen?: () => void;
+}
+
+function requestFullScreen(element: DocumentElementWithFullscreen) {
+  if (element.requestFullscreen) {
     element.requestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  }
+}
+
+function exitFullScreen(doc: DocumentWithFullscreen) {
+  if (doc.exitFullscreen) {
+    doc.exitFullscreen();
+  } else if (doc.webkitExitFullscreen) {
+    doc.webkitExitFullscreen();
+  } else if (doc.mozCancelFullScreen) {
+    doc.mozCancelFullScreen();
+  }
+}
+
+export const toggleFullscreen = (element: DocumentElementWithFullscreen) => {
+  if (isFullScreen()) {
+    exitFullScreen(document);
   } else {
-    document.exitFullscreen();
+    requestFullScreen(element);
   }
 };

76bf707437c0ca163b92e726197e00198fccc47b
Evgeniy.Zaborshchikov <49640784+jacksonrr3@users.noreply.github.com>
2023-01-18 14:24:33 +0400

    feat: 2048-12 авторизация (#24)

---
+++ packages/client/src/processes/routes/ui.tsx
@@ -0,0 +1,16 @@
+import { useStore } from 'effector-react';
+import { $isAuth } from 'processes/layout/model/model';
+import { FC, PropsWithChildren } from 'react';
+import { Navigate } from 'react-router-dom';
+import { routesPath } from './routesPath';
+
+export const PrivateRoute: FC<PropsWithChildren> = ({ children }) => {
+  const isAuth = useStore($isAuth);
+  if (!isAuth) {
+    // temporary log into console
+    console.log('Access denied! User is not authorized.');
+    return <Navigate to={routesPath.signin} />;
+  }
+
+  return <>{children}</>;
+};

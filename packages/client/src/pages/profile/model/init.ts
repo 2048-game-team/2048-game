e913b8eff78a06bb8c47105ca05ae94c238e1a1a
maxmaxk <cooleshov@inbox.ru>
2023-01-18 07:20:09 +0300

    profile api draft

---
+++ packages/client/src/pages/profile/model/init.ts
@@ -0,0 +1,5 @@
+import { sample } from 'effector';
+import { logout } from 'pages/logout/model/model';
+import { logoutFx } from 'pages/logout/model/effects';
+
+sample({ clock: logout, target: logoutFx });
---
+++ packages/client/src/pages/profile/ui.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+/*import { Button, Divider, Form, Modal, Typography } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import { routesPath } from 'processes/routes';
+import './model/init';
+import { logout } from 'pages/logout/model';
+import { useStore } from 'effector-react';
+import { signinFx } from 'pages/signin/model';*/
+
+export const Profile: React.FC = () => {
+  /*const navigate = useNavigate();
+  const loading = useStore(signinFx.pending);
+
+  const handleCancel = () => {
+    navigate(routesPath.home);
+  };
+
+  const onFinish = () => {
+    logout();
+    navigate(routesPath.home);
+  };*/
+
+  return (
+    <>
+      <p>Profile page draft</p>
+    </>
+  );
+};

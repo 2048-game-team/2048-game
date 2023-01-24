8190ff675e6cf75eebf9a91e193c0c3a72553106
Ekaterina Larionova <ekaterinalarionova25@gmail.com>
2023-01-24 13:30:00 +0300

    фото перенесены в public и добавлено кэширование

--- packages/client/src/pages/team/const.tsx
+++ packages/client/src/pages/team/const.tsx
@@ -2,35 +2,35 @@ export const team = [
     {
         id: 1,
         name: 'Дмитрий',
-        src: 'src/pages/team/teamPhotos/dmitry.jpg',
+        src: 'teamPhotos/dmitry.jpg',
         tg: 'Dmi_Lavrov',
         tg_href: 'https://t.me/Dmi_Lavrov'
     },
     {
         id: 2,
         name: 'Евгений',
-        src: 'src/pages/team/teamPhotos/noimage.jpg',
+        src: 'teamPhotos/noimage.jpg',
         tg: 'jacksonrr3',
         tg_href: 'https://t.me/jacksonrr3'
     },
     {
         id: 3,
         name: 'Николай',
-        src: 'src/pages/team/teamPhotos/nikolay.jpg',
+        src: 'teamPhotos/nikolay.jpg',
         tg: 'kamenskiyyyy',
         tg_href: 'https://t.me/kamenskiyyyy'
     },
     {
         id: 4,
         name: 'Максим',
-        src: 'src/pages/team/teamPhotos/noimage.jpg',
+        src: 'teamPhotos/noimage.jpg',
         tg: 'maxmax_k',
         tg_href: 'https://t.me/maxmax_k',
     },
     {
         id: 5,
         name: 'Екатерина',
-        src: 'src/pages/team/teamPhotos/ekaterina.jpg',
+        src: 'teamPhotos/ekaterina.jpg',
         tg: 'eelarionova',
         tg_href: 'https://t.me/eelarionova',
     },

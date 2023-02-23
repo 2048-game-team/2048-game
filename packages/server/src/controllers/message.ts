43b7e37c5f1f499fd98695797c8e281092c869fa
e.zaborshchikov <e.zaborshchikov@yandex.ru>
2023-02-23 15:42:28 +0300

    add route handlers

--- packages/server/src/controllers/message.ts
+++ packages/server/src/controllers/message.ts
@@ -1,15 +1,35 @@
 import type { Handler } from 'express';
 import { Message } from '../models/message';
 
-class MessageController { 
-  getAll: Handler = async (_, res) => {
-    const topics = await Message.findAll()
-    return res.status(200).json(topics)
-  }
-  
-  // getById:Handler = async () => { }
+class MessageController {
+  getAll: Handler = async (_, res, next) => {
+    try {
+      const messages = await Message.findAll();
+      res.status(200).json(messages);
+    } catch (err) { 
+      next(err)
+    } 
+  };
 
-  // createNew:Handler = async () => { }
+  getById: Handler = async (req, res, next) => {
+    try {
+      const { id } = req.query;
+      const message = await Message.findOne({ where: { id } });
+      res.status(200).json(message);
+    } catch (err) {
+      next(err);
+    }  
+  };
+
+  createNew: Handler = async (req, res, next) => {
+    try {
+      const { content, created_at, userId } = req.body;
+      const newMessage = await Message.create({ content, created_at, userId });
+      res.status(200).json(newMessage)
+    } catch (err) {
+      next(err);
+    } 
+  };
 
   // deleteById:Handler = async () => { }
 }
--- packages/server/src/controllers/topic.ts
+++ packages/server/src/controllers/topic.ts
@@ -2,16 +2,24 @@ import type { Handler } from 'express';
 import { Topic } from '../models/topic';
 
 class TopicController {
-  getAll: Handler = async (_, res) => {
-    const topics = await Topic.findAll();
-    return res.status(200).json(topics);
+  getAll: Handler = async (_, res, next) => {
+    try {
+      const topics = await Topic.findAll();
+      res.status(200).json(topics);
+    } catch (err) { 
+      next(err);
+    } 
   };
 
   // getById: Handler = async () => {};
 
-  createNew: Handler = async (req, _) => {
-    const { titile, content, created_at, userId } = req.body;
-    console.log(titile, content, created_at, userId);
+  createNew: Handler = async (req, _, next) => {
+    try {
+      const { titile, content, created_at, userId } = req.body;
+      console.log(titile, content, created_at, userId);
+    } catch (err) { 
+      next(err);
+    }
   };
 
   // deleteById: Handler = async () => {};

0dc16e14e7a28d1f8e51aaad4ef2463d1e2fa2f5
e.zaborshchikov <e.zaborshchikov@yandex.ru>
2023-03-08 12:54:38 +0300

    fix error handler

--- packages/server/src/middlewares/error.ts
+++ packages/server/src/middlewares/error.ts
@@ -1,6 +1,6 @@
 import type { ErrorRequestHandler } from 'express';
 
-export const errorMiddleware: ErrorRequestHandler = (err, _, res) => {
+export const errorMiddleware: ErrorRequestHandler = (err, _, res, __) => {
   console.log(`Error is occured! ${err}`);
   return res.status(500).json({ err });
 };

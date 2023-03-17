68fc8983e7806e908fd516637b1473c23fae45e8
Nikolay Kamenev <kamenik1@icloud.com>
2023-03-17 18:41:43 +0300

    Update const.ts

--- packages/client/const.ts
+++ packages/client/const.ts
@@ -3,4 +3,4 @@ export const YANDEX_OAUTH_URL = 'https://oauth.yandex.ru/authorize';
 export const YANDEX_OAUTH_REDIRECT_URL =
   process.env.VITE_GAME_URL || 'http://localhost:3000';
 export const SERVER_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : origin;
+  process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : '';

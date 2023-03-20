3ee1fc1934594faa2d9958df74c8760ae1f28833
max <cooleshov@inbox.ru>
2023-03-20 13:32:58 +0300

    fix format

--- packages/client/src/app/providers/withRouter.tsx
+++ packages/client/src/app/providers/withRouter.tsx
@@ -1,6 +1,6 @@
 import { FC, PropsWithChildren } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { BASE_URL } from "client/const";
+import { BASE_URL } from 'client/const';
 
 export const WithRouter: FC<PropsWithChildren> = ({ children }) => {
   return <BrowserRouter basename={BASE_URL}>{children}</BrowserRouter>;

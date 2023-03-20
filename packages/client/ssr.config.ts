0f9a2785deb3372cb1cc870be67bd46140429d7c
max <cooleshov@inbox.ru>
2023-03-20 17:46:19 +0300

    test not root base_url

--- packages/client/ssr.config.ts
+++ packages/client/ssr.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_ROOT_PATH,
+  base: `${process.env.VITE_ROOT_PATH}/`,
   plugins: [react(), tsconfigPaths()],
   build: {
     lib: {
--- packages/server/src/consts.ts
+++ packages/server/src/consts.ts
@@ -1,2 +1,1 @@
-export const BASE_URL = process.env.VITE_ROOT_PATH || '/';
 export const API_URL = '/api/v1';
